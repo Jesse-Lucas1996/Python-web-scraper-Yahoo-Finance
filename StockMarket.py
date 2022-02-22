bcaad8d25d2c280792c86296affcc543a96bf0f9
Jesse-Lucas1996 <61496789+Jesse-Lucas1996@users.noreply.github.com>
2022-02-22 14:54:28 +0800

    Update StockMarket.py

--- StockMarket.py
+++ StockMarket.py
@@ -10,34 +10,34 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-
-def parsePrice():
-    r=requests.get('https://au.finance.yahoo.com/quote/GOOG?p=GOOG')    
-    soup=bs4.BeautifulSoup(r.text,"lxml")
-    price=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    return price
-
-def parsePrice2():
-    r=requests.get('https://au.finance.yahoo.com/quote/TSLA?p=TSLA')    
-    soup=bs4.BeautifulSoup(r.text, "lxml")
-    price2=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    return price2
-
-def parsePrice3():
-    r=requests.get('https://au.finance.yahoo.com/quote/AAPL?p=AAPL')    
-    soup=bs4.BeautifulSoup(r.text, "lxml")
-    price3=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    return price3
-
-def parsePrice4():
-    r=requests.get('https://au.finance.yahoo.com/quote/CCL.AX?p=CCL.AX')   
-    soup=bs4.BeautifulSoup(r.text, "lxml")
-    price4=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    return price4
-
-
-
 while True:
+    def parsePrice():
+        r=requests.get('https://au.finance.yahoo.com/quote/GOOG?p=GOOG')    
+        soup=bs4.BeautifulSoup(r.text,"lxml")
+        price=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+        return price
+
+    def parsePrice2():
+        r=requests.get('https://au.finance.yahoo.com/quote/TSLA?p=TSLA')    
+        soup=bs4.BeautifulSoup(r.text, "lxml")
+        price2=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+        return price2
+
+    def parsePrice3():
+        r=requests.get('https://au.finance.yahoo.com/quote/AAPL?p=AAPL')    
+        soup=bs4.BeautifulSoup(r.text, "lxml")
+        price3=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+        return price3
+
+    def parsePrice4():
+        r=requests.get('https://au.finance.yahoo.com/quote/CCL.AX?p=CCL.AX')   
+        soup=bs4.BeautifulSoup(r.text, "lxml")
+        price4=soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+        return price4
+
+
+
+
 
     print('The current price for Alphabet INC (GOOGLE) according to Yahoo Finance is: ' +str(parsePrice()))
 
